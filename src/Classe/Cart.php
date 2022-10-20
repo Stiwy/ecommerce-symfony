48f603f26570932585747943be1909f4aa9e80c0
Stiwy Caritey <stiwy.caritey@gmail.com>
2022-10-20 06:36:00 +0200

    Create the cart page

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -38,4 +38,39 @@ class Cart
     {
         $this->session->remove('cart');
     }
+
+    public function addQuantity(int $id)
+    {
+        $cart = $this->session->get('cart');
+
+        if ((!empty($cart[$id])))
+            $cart[$id]++;
+
+        $this->session->set('cart', $cart);
+    }
+
+    public function downQuantity(int $id)
+    {
+        $cart = $this->session->get('cart');
+
+        if ((!empty($cart[$id]))) {
+            if  ($cart[$id] > 1)
+                $cart[$id]--;
+            else
+                unset($cart[$id]);
+        }
+
+        $this->session->set('cart', $cart);
+    }
+
+    public function removeProduct(int $id)
+    {
+
+        $cart = $this->session->get('cart');
+
+        if ((!empty($cart[$id])))
+            unset($cart[$id]);
+
+        $this->session->set('cart', $cart);
+    }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/mon-panier', name: 'app_cart')]
-    public function index(Cart $cart): Response
+    public function index(Cart $cart, ProductRepository $productRepository): Response
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        $cartComplete = [];
+
+        foreach ($cart->get() as $key => $value) {
+            $cartComplete[] = [
+                'product' => $productRepository->findOneBy(['id' => $key]),
+                'quantity' => $value
+            ];
+        }
+
+        return $this->render('cart/index.html.twig', compact('cartComplete'));
     }
 
     #[Route('/mon-panier/ajouter/{id}', name: 'app_cart_add', methods: 'GET')]
@@ -30,5 +39,29 @@ class CartController extends AbstractController
         $cart->remove();
 
         return $this->redirectToRoute('app_product');
+    }
+
+    #[Route('/mon-panier/plus/{id}', name: 'app_cart_add_quantity', methods: 'GET')]
+    public function addQuantity(Cart $cart, $id): Response
+    {
+        $cart->addQuantity($id);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/mon-panier/moins/{id}', name: 'app_cart_down_quantity', methods: 'GET')]
+    public function downQuantity(Cart $cart, $id): Response
+    {
+        $cart->downQuantity($id);
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+    #[Route('/mon-panier/supprimer/{id}', name: 'app_cart_delete_product', methods: 'GET')]
+    public function removeProduct(Cart $cart, $id): Response
+    {
+        $cart->removeProduct($id);
+
+        return $this->redirectToRoute('app_cart');
     }
 }
